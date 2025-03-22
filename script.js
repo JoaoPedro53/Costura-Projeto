3bc7e08246ed70576fde1d75e7bfd74e9c3410e1
joao <joao@gamil.com>
2025-03-22 00:31:51 -0300

    Front-Menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirMenu () {
+   document.getElementById("menu").style.width = '230px';
+   document.getElementById("conteudo").style.marginLeft = '230px';
+}
+function fecharMenu () {
+    document.getElementById("menu").style.width = '0px';
+    document.getElementById("conteudo").style.marginLeft = '0px';
+ }
